001bc43e4881c7d7d409b34f1a531a2d728e5ad0
Eddy Soma <esoma92.dev@gmail.com>
2019-02-21 15:42:02 -0500

    write the db utility class

---
+++ src/main/java/io/esoma/utility/DatabaseUtility.java
@@ -0,0 +1,145 @@
+package io.esoma.utility;
+
+/**
+ * 
+ * The Utility class with static methods that return database related values and
+ * objects.
+ * 
+ * @author Eddy Soma
+ *
+ */
+public class DatabaseUtility {
+
+	/**
+	 * 
+	 * Gets the environmental variables needed to establish a connection to the
+	 * Oracle Database.
+	 * 
+	 * @return the string array containing the database variables.
+	 */
+	private static String[] getOracleDBVariables() {
+
+		return System.getenv("KHR_AGGRE").split(";@");
+
+	}
+
+	/**
+	 * 
+	 * Gets the class name for the Oracle Database driver.
+	 * 
+	 * @return the driver class name.
+	 */
+	public static String getOracleDBDriverClassName() {
+
+		return getOracleDBVariables()[0];
+
+	}
+
+	/**
+	 * 
+	 * Gets the url string of the Oracle Database used by the application.
+	 * 
+	 * @return the url string.
+	 */
+	public static String getOracleDBUrl() {
+
+		return getOracleDBVariables()[1];
+
+	}
+
+	/**
+	 * 
+	 * Gets the username of the Oracle Database connection.
+	 * 
+	 * @return the username string.
+	 */
+	public static String getOracleDBUsername() {
+
+		return getOracleDBVariables()[2];
+
+	}
+
+	/**
+	 * 
+	 * Gets the password of the Oracle Database connection.
+	 * 
+	 * @return the password string.
+	 */
+	public static String getOracleDBPassword() {
+
+		return getOracleDBVariables()[3];
+
+	}
+
+	/**
+	 * 
+	 * Gets the environmental variables needed to connect to the H2 in-memory
+	 * database.
+	 * 
+	 * @return the string array containing the database variables.
+	 */
+	private static String[] getH2DBVariables() {
+
+		return System.getenv("H2_AGGRE").split(";@");
+
+	}
+
+	/**
+	 * 
+	 * Gets the class name for the H2 Database driver.
+	 * 
+	 * @return the driver class name.
+	 */
+	public static String getH2DBDriverClassName() {
+
+		return getH2DBVariables()[0];
+
+	}
+
+	/**
+	 * 
+	 * Gets the url string of the H2 Database.
+	 * 
+	 * @return the url string.
+	 */
+	public static String getH2DBUrl() {
+
+		return getH2DBVariables()[1];
+
+	}
+
+	/**
+	 * 
+	 * Gets the username of the H2 Database connection. Returns "sa" if the
+	 * variables is not available.
+	 * 
+	 * @return the username string.
+	 */
+	public static String getH2DBUsername() {
+
+		try {
+			return getH2DBVariables()[2];
+		} catch (Exception e) {
+			return "sa";
+		}
+
+	}
+
+	/**
+	 * 
+	 * Gets the password of the H2 Database connection. Returns an empty string is
+	 * the variable is not available.
+	 * 
+	 * @return the password string.
+	 */
+	public static String getH2DBPassword() {
+
+		try {
+			return getH2DBVariables()[3];
+		} catch (Exception e) {
+			return "";
+		}
+
+	}
+
+}
